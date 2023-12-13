a42d1cc0c46351fc7f5b3e8b4cf36afac7ffee38
DerGrubengraeber <107435066+DerGrubengraeber@users.noreply.github.com>
2023-12-13 03:19:37 +0100

    Utility.cs
    Class for utility functions, currently only contains the prefix matcher.

---
+++ Utility.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CedMod;
+using CedMod.Addons.Events.Interfaces;
+using CedMod.Addons.Events;
+
+namespace EventScheduler
+{
+    public class Utility
+    {
+        public static IEvent PrefixMatcher(String Prefix)
+        {
+            foreach(IEvent Event in EventManager.AvailableEvents)
+            {
+                if(Prefix == Event.EventPrefix)
+                {
+                    return Event;
+                }                  
+            }
+            return null;
+        }
+    }
+}
