943696247f919dc0574bf3cae03e2eb9b14a707e
DerGrubengraeber <107435066+DerGrubengraeber@users.noreply.github.com>
2023-12-13 03:20:13 +0100

    Update EventHandler.cs
    Added support for the Exclusive Events and Minimum Player options

--- EventHandler.cs
+++ EventHandler.cs
@@ -15,54 +15,92 @@ namespace EventScheduler
     {
         private static Config config => EventScheduler.Instance.Config;
         private int roundsPassed = 0;
-        private int spacerRounds = config.RoundsBetween-1;
+        private int spacerRounds = config.RoundsBetween - 1;
         private int eventMenge; //no i will not change the name of this :P
         private int eventsCurrent = 0;
         private IEvent lastEvent;
-        Random rnd = new Random();
+        private Random rnd = new Random();
+        private static List<IEvent> AEvents;
+        private bool enoughPlayers = true; //this is horrible but i don't have another fix rn
         public EventHandler()
         {
-            Exiled.Events.Handlers.Server.WaitingForPlayers += PlayerWaiting;
+            Exiled.Events.Handlers.Server.RestartingRound += RoundRestart;
+            Exiled.Events.Handlers.Server.WaitingForPlayers += PlayerWaiting;    
         }
-         ~EventHandler()
+        ~EventHandler()
         {
+            Exiled.Events.Handlers.Server.RestartingRound -= RoundRestart;
             Exiled.Events.Handlers.Server.WaitingForPlayers -= PlayerWaiting;
         }
+
+        public void RoundRestart()
+        {
+            if(Player.List.Count >= config.MinPlayers)
+            {
+                
+                enoughPlayers = true;
+            }
+            else
+            {
+                enoughPlayers = false;
+            }
+        }
+
         public void PlayerWaiting()
         {
-            eventMenge = EventManager.AvailableEvents.Count;
-            if (roundsPassed >= spacerRounds)
+            if (!config.EnableExclusiveEvents)
+            {
+                AEvents = new List<IEvent>(EventManager.AvailableEvents);
+            }
+            else
+            {
+                AEvents = new List<IEvent>();
+                foreach (string prefix in config.ExclusiveEvents)
                 {
-                if (EventManager.AvailableEvents != null && EventManager.CurrentEvent == null)
+                   if(Utility.PrefixMatcher(prefix) != null)
+                    {                      
+                        AEvents.Add(Utility.PrefixMatcher(prefix));
+                    } 
+                    else
+                    {
+                        Log.Error("ERROR no event with the Prefix " + prefix + " Please resolve this issue to avoid issues with the event scheduling");
+                    }
+                }
+            }
+            eventMenge = AEvents.Count;
+            if (roundsPassed >= spacerRounds) 
                 {
+                if (EventManager.AvailableEvents != null && EventManager.CurrentEvent == null && enoughPlayers) 
+                {
+                  
                     roundsPassed = 0;
                     if (!config.Randomize)
                     {
-                        IEvent nextEvent = EventManager.AvailableEvents[eventsCurrent];
+                        IEvent nextEvent = AEvents[eventsCurrent];
                         Server.ExecuteCommand("/Events enable " + nextEvent.EventPrefix + " false");
                         eventsCurrent++;
                         if (eventsCurrent >= eventMenge)
                         {
                             eventsCurrent = 0;
-                        }
+                        } 
                     }
                     else 
                     {
                         int rndN = rnd.Next(0, eventMenge);
-                        if (config.TwoInARow)
+                        if (!config.TwoInARow) 
                         {
-
-                            IEvent nextEvent = EventManager.AvailableEvents[rndN];
-                            while(nextEvent.EventName == lastEvent.EventName)
+                            IEvent nextEvent = AEvents[rndN];
+                            while (nextEvent.EventName == lastEvent.EventName)
                             {
-                                nextEvent = EventManager.AvailableEvents[rndN];
+                                nextEvent = AEvents[rndN];
                             }
                             Server.ExecuteCommand("/Events enable " + nextEvent.EventPrefix + " false");
                             lastEvent = nextEvent;
                         }
                         else
                         {
-                            IEvent nextEvent = EventManager.AvailableEvents[rndN];
+                            
+                            IEvent nextEvent = AEvents[rndN];
                             Server.ExecuteCommand("/Events enable " + nextEvent.EventPrefix + " false");
                            
                         }
@@ -70,7 +108,8 @@ namespace EventScheduler
                 }
                 }
                 else if(EventManager.CurrentEvent == null)
-                {
+                {        
+                
                     roundsPassed++;
                 }
         }
